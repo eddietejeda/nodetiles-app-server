cd3b2567b5c8edced6652aa5dc2eb2e644427ffa
Eddie A Tejeda <eddie@codeforamerica.org>
2013-02-10 21:05:12 -0800

    bring code back from app_server branch

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var nodetiles = require('nodetiles-core'),
     fs = require('fs');
 
 
-var PORT = process.env.PORT || process.argv[2] || 3000;
+var PORT = process.env.PORT || process.argv[2] || 5000;
 var DEBUG = true;
 
 
@@ -17,16 +17,16 @@ var DEBUG = true;
 
 /* Create your map context */
 var map = new nodetiles.Map({
-    projection: "EPSG:4326" // set the projection of the map
+    projection: "EPSG:900913" // set the projection of the map
 });
 
 map.addData(new PostGISSource({
-  connectionString: "tcp://postgres@localhost/neworleans_roads", // required
-  tableName: "neworleans_roads_schema",                              // required
-  geomField: "the_geom",                            // required
+  connectionString: "tcp://postgres@localhost/neworleans_addresses", // required
+  tableName: "addresses",                              // required
+  geomField: "geom",                            // required
   // fields: "speed, shape_len",                        // optional, speeds things up
-  name: "oakland_streets",                                     // optional, uses table name otherwise
-  projection: 'EPSG:4326',                                   // optional, defaults to 4326
+  name: "addresses",                                     // optional, uses table name otherwise
+  projection: 'EPSG:900913',                                   // optional, defaults to 4326
   // projection: 4326,                                   // optional, defaults to 4326
 }));
 
@@ -36,63 +36,30 @@ map.addStyle(fs.readFileSync('./styles/style.mss','utf8'));
 
 
 
-// -122.364,37.6927,-122.0756,37.8945
+//
+// Configure Express routes
+//
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-/* Render out the map to a file */
-map.render({
-  // Make sure your bounds are in the same projection as the map
-  bounds: {minX: -122.364, minY: 37.6927, maxX: -122.0756, maxY: 37.8945},
-  width: 800,   // number of pixels to output
-  height: 400,
-  callback: function(err, canvas) {
-    var file = fs.createWriteStream(__dirname + '/map.png'),
-        stream = canvas.createPNGStream();
-
-    stream.on('data', function(chunk){
-      file.write(chunk);
-    });
-
-    stream.on('end', function(){
-      console.log('Saved map.png!');
-    });
-  }
+app.configure('production', function(){
+  app.use(express.errorHandler());  
 });
 
 
-
-// // Wire up the URL routing
-// app.use('/tiles', nodetiles.route.tilePng({ map: map })); // tile.png
-// app.use('/utfgrids', nodetiles.route.utfGrid({ map: map })); // utfgrids
-// // tile.json: use app.get for the tile.json since we're serving a file, not a directory
-// app.get('/tile.json', nodetiles.route.tileJson({ path: __dirname + '/map/tile.json' }));
-
-
-// //
-// // Configure Express routes
-// //
-// app.configure('development', function(){
-//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  
-//   // Backbone routing
-//   app.use('/assets', express.static(__dirname + '/assets'));
-// });
-
-// app.configure('production', function(){
-//   app.use(express.errorHandler());
-//   io.set('log level', 1); // reduce logging
-  
-//   // Backbone routing: compilation step is included in `npm install` script
-//   app.use('/app', express.static(__dirname + '/dist/release'));
-//   app.use('/assets/js/libs', express.static(__dirname + '/dist/release'));
-//   app.use('/assets/css', express.static(__dirname + '/dist/release'));
-//   app.use(express.static(__dirname + '/public'));
-// });
+// Wire up the URL routing
+app.use('/tiles', nodetiles.route.tilePng({ map: map })); // tile.png
+app.use('/utfgrids', nodetiles.route.utfGrid({ map: map })); // utfgrids
+// tile.json: use app.get for the tile.json since we're serving a file, not a directory
+app.get('/tile.json', nodetiles.route.tileJson({ path: __dirname + '/map/tile.json' }));
 
 
-// // 1. Serve Index.html
-// app.get('/', function(req, res) {
-//   res.sendfile(__dirname + '/index.html');
-// });
+
+// 1. Serve Index.html
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
     
-// app.listen(PORT);
-// console.log("Express server listening on port %d in %s mode", PORT, app.settings.env);
+app.listen(PORT);
+console.log("Express server listening on port %d in %s mode", PORT, app.settings.env);
