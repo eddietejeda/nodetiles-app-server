d7f0768d83eab9988a7bf55895c257278e3dd35f
Eddie A Tejeda <eddie@codeforamerica.org>
2013-02-18 01:04:23 -0600

    create a database connection helper

--- app.js
+++ app.js
@@ -1,67 +1,42 @@
-  /* Set up the libraries */
+// Set up the libraries
 var nodetiles = require('nodetiles-core'),
     PostGISSource = nodetiles.datasources.PostGIS,
-    GeoJsonSource = nodetiles.datasources.GeoJson,
     Projector = nodetiles.projector,
     path = require('path'),
     express = require('express'),
     app = module.exports = express(),
     fs = require('fs'),
     TileJSON = require('./app/tilejson');
+    AddressDatabase = require('./app/database');
     
 
-
+// Some general settings
 var PORT = process.env.PORT || process.argv[2] || 5000;
 var DEBUG = true;
 
 
-
-
-/* Create your map context */
+// Create your map context
 var map = new nodetiles.Map();
-var tilejson = new TileJSON({
-  mapCenter: [90.0761,29.9531,17],
-  mapName: "neworleans"
-});
 
 
-var db_connection = '';
-if(process.env.PRODUCTION){
-  connectionString = "tcp://" + process.env.DATABASE_URL;
-}
-else{
-  connectionString = "tcp://postgres@localhost/blightstatus_be";
-}
+// Generate the TileJSON
+var tilejson = new TileJSON({ mapCenter: [90.0761,29.9531,17],
+                              mapName: "neworleans" });
 
-map.addData(new PostGISSource({
-  connectionString: connectionString, // required
-  tableName: "addresses",  // required
-  geomField: "point",  // required
-  // fields: "speed, shape_len",  // optional, speeds things up
-  name: "addresses",    // optional, uses table name otherwise
-  projection: 'EPSG:900913',   // optional, defaults to 4326
-  requestParams: {
-    open_cases : { 
-      required : false,  //defaults to false, @TODO implement required conditions
-      statement: ' open_count >= :open_cases '
-    },
-    closed_cases : { 
-      required : false,
-      statement: ' closed_count >= :closed_cases '
-    }
-  }
+// Automatically configure the database
+var database = new AddressDatabase({ connectionString: 'postgres@localhost/blightstatus_be' });
 
-}));
+// console.log(database);
+
+// Connect the database to the map
+map.addData(database);
   
 
-/* Link your Carto stylesheet */
+// Link your Carto stylesheet
 map.addStyle(fs.readFileSync('./styles/style.mss','utf8'));
 
 
-
-//
 // Configure Express Express
-//
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -77,11 +52,11 @@ app.use('/utfgrids', nodetiles.route.utfGrid({ map: map })); // utfgrids
 app.get('/tile.json', nodetiles.route.tileJson(tilejson));
 
 
-
-// 1. Serve Index.html
+// Serve a basic index.html
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-    
+
+// Set up server to listen in PORT
 app.listen(PORT);
 console.log("Express server listening on port %d in %s mode", PORT, app.settings.env);
---
+++ app/database.js
@@ -0,0 +1,47 @@
+var nodetiles = require('nodetiles-core'),
+    PostGISSource = nodetiles.datasources.PostGIS;
+
+var AddressDatabase = function(options) {
+
+  //defaults
+  this.connectionString = '';
+  if(process.env.PRODUCTION){
+    this.connectionString = "tcp://" + process.env.DATABASE_URL;
+  }
+  else if (options.connectionString){
+    this.connectionString = "tcp://" + options.connectionString;
+  }
+  else{
+    throw new Error("You must set options.connectionString");
+  }
+  
+
+  var connection = new PostGISSource({
+    connectionString: this.connectionString, // required
+    tableName: "addresses",  // required
+    geomField: "point",  // required
+    // fields: "speed, shape_len",  // optional, speeds things up
+    name: "addresses",    // optional, uses table name otherwise
+    projection: 'EPSG:900913',   // optional, defaults to 4326
+    requestParams: {
+      open_cases : { 
+        required : false,  //defaults to false, @TODO implement required conditions
+        statement: ' open_count >= :open_cases '
+      },
+      closed_cases : { 
+        required : false,
+        statement: ' closed_count >= :closed_cases '
+      }
+    }
+  });
+  
+  // console.log(connection);
+  return connection;
+}
+
+
+module.exports = AddressDatabase;
+
+
+
+
