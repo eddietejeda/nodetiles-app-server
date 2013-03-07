852c0575fe846612d03e86c8ceec6f38341d9f5a
Eddie A Tejeda <eddie@codeforamerica.org>
2013-03-07 14:49:59 -0800

    define default params

--- app/tilejson.js
+++ app/tilejson.js
@@ -22,7 +22,7 @@ var TileJSON = function(options) {
   var options = options || {};
 
   //defaults
-  this.template = "{{{geopin}}} {{{street_id}}} {{{house_num}}} {{{street_name}}} {{{street_type}}} {{{address_long}}} {{{case_district}}} {{{status}}} {{{created_at}}} {{{updated_at}}} {{{point}}} {{{parcel_id}}} {{{assessor_url}}} {{{neighborhood_id}}} {{{latest_type}}} {{{latest_id}}} {{{double_id}}} {{{agency_id}}} {{{source_address_id}}} {{{source_parcel_id}}} {{{source_street_id}}} {{{open_count}}} {{{closed_count}}}"
+  this.template = '{{{geopin}}} {{{street_id}}} {{{house_num}}} {{{street_name}}} {{{street_type}}} {{{address_long}}} {{{case_district}}} {{{status}}} {{{created_at}}} {{{updated_at}}} {{{point}}} {{{parcel_id}}} {{{assessor_url}}} {{{neighborhood_id}}} {{{latest_type}}} {{{latest_id}}} {{{double_id}}} {{{agency_id}}} {{{source_address_id}}} {{{source_parcel_id}}} {{{source_street_id}}} {{{open_count}}} {{{closed_count}}}'
   this.maxZoom = 20;
   this.minZoom = 15;
   this.mapId = 'map';
@@ -50,30 +50,30 @@ TileJSON.prototype = {
 
   set : function(options){
     var options = options || {};
-    console.log(options);
-    // required user options
-    // if (!options.agencyName) {
-    //   throw new Error("You must set options.agencyName");
-    // }
-    // else{
-    //   this.mapName = options.agencyName;
-    // }
-    // 
-    // if (!options.mapCenter) {
-    //   throw new Error("You must set options.mapCenter");
-    // }
-    // else{
-    //   this.mapCenter = options.mapCenter;    
-    // }
-    // 
-    // 
-    // if (!options.domain) {
-    //   throw new Error("You must set options.domain");
-    // }
-    // else{
-    //   this.domain = options.domain;    
-    // }
-    // 
+
+    if (!options.agencyName) {
+      throw new Error("You must set options.agencyName");
+    }
+    else{
+      this.agencyName = options.agencyName;
+      this.mapName = options.agencyName;
+    }
+    
+    if (!options.mapCenter) {
+      throw new Error("You must set options.mapCenter");
+    }
+    else{
+      this.mapCenter = options.mapCenter;    
+    }
+    
+    
+    if (!options.domain) {
+      throw new Error("You must set options.domain");
+    }
+    else{
+      this.domain = options.domain;    
+    }
+    
     return this;
        
   },
@@ -84,7 +84,7 @@ TileJSON.prototype = {
         "center"      : this.mapCenter, 
         "description" : "Lovingly crafted with Node and node-canvas.",
         "attribution" : "Data by Data",
-        "grids"       :  [this.domain + "/" + this.mapName +  "/utfgrids/{z}/{x}/{y}.json"],
+        "grids"       :  [this.domain + "/utfgrids/" + this.mapName +  "/{z}/{x}/{y}.json"],
         "legend"      : "<div style=\"text-align:center;\"><div style=\"font:12pt/16pt Georgia,serif;\">San Francisco</div><div style=\"font:italic 10pt/16pt Georgia,serif;\">by BlightStatus</div></div>",
         "id"          : this.mapId,
         "maxzoom"     : this.maxZoom,
@@ -92,14 +92,13 @@ TileJSON.prototype = {
         "name"        : this.mapName,
         "scheme"      : "xyz",
         "template"    : this.template,    
-        "tiles"       : [this.domain + "/" + this.mapName +  "/tiles/{z}/{x}/{y}.png"],
+        "tiles"       : [this.domain + "/tiles/" + this.mapName +  "/{z}/{x}/{y}.png"],
         "version"     : "0.1.0",
         "webpage"     : "http://github.com/eddietejeda/nodetiles-init"
     }
   }
   
 }
-
 
 
 module.exports = TileJSON;
